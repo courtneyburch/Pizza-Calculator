25bff97dabfe02a01bd3cbd6cbe90c0d157d343d
courtneyburch <courtnbur@yahoo.com>
2022-03-09 14:05:06 -0500

    update results display

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//function to show the relevant dimensions for round/square and hide the others
 function show_round_dim_pizza1(){
     document.querySelector(".round1").classList.remove("invisible");
     document.querySelector(".rectangle1").classList.add("invisible");
@@ -8,15 +8,6 @@ function show_round_dim_pizza2(){
     document.querySelector(".rectangle2").classList.add("invisible");
 }
 
-function show_square_dim_pizza1(){
-    document.querySelector(".round1").classList.add("invisible");
-    document.querySelector(".rectangle1").classList.add("invisible");
-}
-
-function show_square_dim_pizza2(){
-    document.querySelector(".round2").classList.add("invisible");
-    document.querySelector(".rectangle2").classList.add("invisible");
-}
 function show_rect_dim_pizza1(){
     document.querySelector(".round1").classList.add("invisible");
     document.querySelector(".rectangle1").classList.remove("invisible");
@@ -26,7 +17,7 @@ function show_rect_dim_pizza2(){
     document.querySelector(".rectangle2").classList.remove("invisible");
 }
 
-//function to calculate area of both pizzas and compare them
+//function to calculate area of both pizzas and compare them, then display result
 document.querySelector("#calculate").addEventListener("click", calc);
 function calc(){
 
@@ -41,7 +32,7 @@ function calc(){
     let area2 = 0;
     const shape2 = document.querySelector('input[name="shape2"]:checked').value; //get dimensions of pizza2
     const qty2 = document.querySelector('input[name="pizza2_qty"]').value; //get quantity of pizza 2
-    const price2 = document.querySelector('input[name="pizza1_price"]').value; //get price of pizza 2
+    const price2 = document.querySelector('input[name="pizza2_price"]').value; //get price of pizza 2
     let totalArea2 = 0; //total area of pizza 2
     let totalCost2 = 0; //total cost of pizza 2
     let unitCost2 = 0; //cost per suqare inch of pizza 2
@@ -50,11 +41,11 @@ function calc(){
 
     //calculate area of pizza 1
     if(shape1 == "round"){
-        pizza1Diameter = document.getElementById("pizza1_round_diam").value;
+        pizza1Diameter = document.querySelector('input[name="pizza1_round_diam"]').value;
         area1 = Math.PI * pizza1Diameter;
-    } else{
-        pizza1Length = document.getElementById("pizza1_rectangle_length").value;
-        pizza1Width = document.getElementById("pizza1_rectangle_width").value;
+    } else if(shape1 == "rectangle"){
+        pizza1Length = document.querySelector('input[name="pizza1_rectangle_length"]').value;
+        pizza1Width = document.querySelector('input[name="pizza1_rectangle_width"]').value;
         area1 = pizza1Length * pizza1Width;
     }
 
@@ -69,12 +60,12 @@ function calc(){
 
     //calculate area of pizza 2
     if(shape2 == "round"){
-        pizza2Diameter = document.getElementById("pizza2_round_diam").value;
-        area2 = (Math.PI * pizza2Diameter).toFixed(1);
-    } else{
-        pizza2Length = document.getElementById("pizza2_rectangle_length").value;
-        pizza2Width = document.getElementById("pizza2_rectangle_width").value;
-        area2 = (pizza2Length * pizza2Width).toFixed(1);
+        pizza2Diameter = document.querySelector('input[name="pizza2_round_diam"]').value;
+        area2 = (Math.PI * pizza2Diameter).toFixed(2);
+    } else if(shape2 == "rectangle"){
+        pizza2Length = document.querySelector('input[name="pizza2_rectangle_length"]').value;
+        pizza2Width = document.querySelector('input[name="pizza2_rectangle_width"]').value;
+        area2 = (pizza2Length * pizza2Width).toFixed(2);
     }
     //calculate total area of pizza 2
     totalArea2 = area2 * qty2;
@@ -87,13 +78,13 @@ function calc(){
 
     //compare the two unit costs and decide which one is the best deal. Populate result string with correct outcome
     if(unitCost1 < unitCost2){
-        result = `Pizza 1 has a cost of ${unitCost1} per square inch and pizza 2 has a cost of ${unitCost2} per square inch. Pizza 1 is a better value.`
+        result = `Pizza 1 has a cost of $${unitCost1} per square inch and Pizza 2 has a cost of $${unitCost2} per square inch. <strong>Pizza 1 is a better value.</strong>`
     } else {
-        result = `Pizza 2 has a cost of ${unitCost2} per square inch and pizza 1 has a cost of ${unitCost1} per square inch. Pizza 2 is a better value`
+        result = `Pizza 1 has a cost of $${unitCost1} per square inch and Pizza 2 has a cost of $${unitCost2} per square inch. <strong>Pizza 2 is a better value.</strong>`
     }
     //append result to the main container
-    let displayResult = document.createElement('p'); //create a paragraph to display the result
+    let displayResult = document.createElement('div'); //create a div to display the result
+    displayResult.classList.add('results'); //add class name for styling
     displayResult.innerHTML = result; //append the result template
-    console.log(displayResult);
-    document.getElementById('main_container').appendChild(displayResult) //append the the bottom of the main container
+    document.getElementById('container').appendChild(displayResult) //append the the bottom of the main container
 }
