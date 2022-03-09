fbf77f2134fa27045740d9139d24941121430916
courtneyburch <courtnbur@yahoo.com>
2022-03-09 10:45:55 -0500

    Remove square pizza option since it's redundant. Remove js code that didn't work

--- script.js
+++ script.js
@@ -1,63 +1,29 @@
 
-// document.querySelector("#pizza1_shape").addEventListener("click", showDimensions)
-// document.querySelector("#pizza2_shape").addEventListener("click", showDimensions)
-
-// function showDimensions(){
-//     const pizza1Shape = document.querySelector('input[name="shape1"]:checked').value;
-//     const pizza2Shape = document.querySelector('input[name="shape2"]:checked').value;
-//     console.log(pizza1Shape);
-//     //show the relevant input fields depending on which shape is selected
-//     if(pizza1Shape === "round"){
-//         //show input for round dimensions and hide the others
-//         document.querySelector(".round").classList.remove("invisible");
-//         document.querySelector(".rectangle").classList.add("invisible");
-//         document.querySelector(".square").classList.add("invisible");
-        
-//         //show input for square dimension and hide the others
-//     } else if(pizza1Shape === "square"){
-//         document.querySelector(".round").classList.add("invisible");
-//         document.querySelector(".rectangle").classList.add("invisible");
-//         document.querySelector(".square").classList.remove("invisible");
-
-//         //show input for rectangular dimension and hide the others
-//     } else {
-//         document.querySelector(".round").classList.add("invisible")
-//         document.querySelector(".rectangle").classList.remove("invisible")
-//         document.querySelector(".square").classList.add("invisible")
-//     }
-// };
-
 function show_round_dim_pizza1(){
     document.querySelector(".round1").classList.remove("invisible");
     document.querySelector(".rectangle1").classList.add("invisible");
-    document.querySelector(".square1").classList.add("invisible");
 }
 function show_round_dim_pizza2(){
     document.querySelector(".round2").classList.remove("invisible");
     document.querySelector(".rectangle2").classList.add("invisible");
-    document.querySelector(".square2").classList.add("invisible");
 }
 
 function show_square_dim_pizza1(){
     document.querySelector(".round1").classList.add("invisible");
     document.querySelector(".rectangle1").classList.add("invisible");
-    document.querySelector(".square1").classList.remove("invisible");
 }
 
 function show_square_dim_pizza2(){
     document.querySelector(".round2").classList.add("invisible");
     document.querySelector(".rectangle2").classList.add("invisible");
-    document.querySelector(".square2").classList.remove("invisible");
 }
 function show_rect_dim_pizza1(){
     document.querySelector(".round1").classList.add("invisible");
     document.querySelector(".rectangle1").classList.remove("invisible");
-    document.querySelector(".square1").classList.add("invisible");
 }
 function show_rect_dim_pizza2(){
     document.querySelector(".round2").classList.add("invisible");
     document.querySelector(".rectangle2").classList.remove("invisible");
-    document.querySelector(".square2").classList.add("invisible");
 }
 
 // document.querySelector(".calculate_area").onclick = function(shape){
